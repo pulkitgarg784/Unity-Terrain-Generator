c224d3ea0edc4278b81221e54a278cea0c8d58a1
pulkitgarg784 <33174851+pulkitgarg784@users.noreply.github.com>
2020-07-14 10:03:23 +0530

    Create Random perlin noise for the terrain generator to work off.

--- Assets/TerrainGenerator.cs
+++ Assets/TerrainGenerator.cs
@@ -8,8 +8,31 @@ public class TerrainGenerator : MonoBehaviour
     public int height;
     public float noiseScale;
 
-    public void GenerateTerrain()
+    public Renderer texturerenderer;
+
+    public void GenerateMap()
     {
-        float[,] noisemap = NoiseGenerator.Generate(width, height, noiseScale);
+        float[,] noiseMap = NoiseGenerator.Generate(width, height, noiseScale);
+        GenerateTerrain(noiseMap);
+    }
+
+    public void GenerateTerrain(float[,] noisemap)
+    {
+        int width = noisemap.GetLength(0);
+        int height = noisemap.GetLength(1);
+        Texture2D texture = new Texture2D(width, height);
+        Color[] colormap = new Color[width * height];
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                colormap[y * width + x] = Color.Lerp(Color.black, Color.white, noisemap[x, y]);
+            }
+        }
+
+        texture.SetPixels(colormap);
+        texture.Apply();
+        texturerenderer.sharedMaterial.mainTexture = texture;
+        texturerenderer.transform.localScale = new Vector3(width, 1, height);
     }
 }
